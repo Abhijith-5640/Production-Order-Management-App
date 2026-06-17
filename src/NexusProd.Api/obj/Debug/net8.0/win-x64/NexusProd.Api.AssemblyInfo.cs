f9f8e0ba9250bb17ed6eb76480c58e9bdedc1c22
Abhijith T <abhijith.cypheron@outlook.com>
2026-06-17 18:15:34 +0530

    C

--- src/NexusProd.Api/obj/Debug/net8.0/win-x64/NexusProd.Api.AssemblyInfo.cs
+++ src/NexusProd.Api/obj/Debug/net8.0/win-x64/NexusProd.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexusProd.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82353b1a4145a62dc25a2ae1a7be7e2a008507da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c15d4db094b3cda692680c542b795b6fb74fe5")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexusProd.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexusProd.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
